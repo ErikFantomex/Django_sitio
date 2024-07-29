4003282705eb8be905d7b48e9b0cf30c6d482975
ErikFantomex <a217213318@unison.mx>
2024-07-29 13:08:50 -0700

    se agrego Postresql

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -91,7 +91,20 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+#Base de datos neon
+CONN_MAX_AGE = config("CONN_MAX_AGE",cast=int,default=30)
+DATABASE_URL = config("DATABASE_URL",cast=str)
 
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(
+            default = DATABASE_URL,
+            conn_max_age = 30,
+            conn_health_checks=True,
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
