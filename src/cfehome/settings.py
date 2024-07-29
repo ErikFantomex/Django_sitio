0544c1e67b7a513ac9bbc53200ea1a42b1639b80
ErikFantomex <a217213318@unison.mx>
2024-07-28 20:53:07 -0700

    listo

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = 'django-insecure-40$=8%3!cf#(bs*fb=*pn(nhao88hb!gfp*^$%myy&)9x!azk*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app" # https://saas.prod.railway.app
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 
 # Application definition
