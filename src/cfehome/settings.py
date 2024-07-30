c45981dbfdcc1cc77e8f341767038b9ddf9d1199
ErikFantomex <a217213318@unison.mx>
2024-07-29 18:26:40 -0700

    Feats: update dockerfile add functions

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 from decouple import config
 from pathlib import Path
-import os
+#import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #my apps
+    'commando',
     'visits',
 ]
 
@@ -92,8 +93,8 @@ DATABASES = {
     }
 }
 #Base de datos neon
-CONN_MAX_AGE = config("CONN_MAX_AGE",cast=int,default=30)
-DATABASE_URL = config("DATABASE_URL",cast=str)
+CONN_MAX_AGE = config("CONN_MAX_AGE",cast=int,default=300)
+DATABASE_URL = config("DATABASE_URL",default=None)
 
 
 if DATABASE_URL is not None:
@@ -101,7 +102,7 @@ if DATABASE_URL is not None:
     DATABASES = {
         'default': dj_database_url.config(
             default = DATABASE_URL,
-            conn_max_age = 30,
+            conn_max_age = CONN_MAX_AGE,
             conn_health_checks=True,
         )
     }
@@ -140,7 +141,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = "static/"
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+# source(s) for python manage.py collectstatic 
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
+
+# output for python manage.py collectstatic 
+# local cdn
+STATIC_ROOT = BASE_DIR / "local-cdn"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
